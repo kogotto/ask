dbcb09004f290612fad008392c8c69da77c7997a
Didenko Yakov <kogotto@gmail.com>
2014-07-04 10:32:04 +0400

    templates

--- questions/urls.py
+++ questions/urls.py
@@ -4,5 +4,8 @@ from questions import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.index, name = 'index')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^(?P<id>\d+)/$', views.detail, name = 'detail'),
+    url(r'^(?P<id>\d+)/results/$', views.results, name = 'results'),
+    url(r'^(?P<id>\d+)/votes/$', views.votes, name = 'votes')
 )
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from questions.models import Question
 
 def index(request):
-    return HttpResponse("Hello, motherfucker")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('questions/index.html')
+    context = RequestContext(request, {
+        'latest_questions': latest_questions,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, id):
+    question = Question.objects.get(pk = id)
+    return HttpResponse("Question %s" % question.text)
+
+def results(request, id):
+    return HttpResponse("Results of question %s" % id)
+
+def votes(request, id):
+    return HttpResponse("Votes for question %s" % id)
