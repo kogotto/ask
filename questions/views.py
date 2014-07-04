80efb1a60926ffc8a25a32bf7f68016d1e11f5e4
Didenko Yakov <kogotto@gmail.com>
2014-07-04 09:03:48 +0400

    fix

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from djungo.http import HttpResponse
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("Hello, motherfucker")
